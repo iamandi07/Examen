c204cd38de13db993084056663be35969634d40c
Szabi <szabiadam@yahoo.com>
2020-07-06 20:50:27 +0300

    data from back-end to front-end
    Fetch Data: showing the data from database in front-end

--- Examen/ClientApp/src/app/app.module.ts
+++ Examen/ClientApp/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AngularMaterialModule } from './shared/angular-material.module'
+
+import { CoreModule } from './core/core.module'
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'my-route', component: CounterComponent },
+];
 
 @NgModule({
   declarations: [
@@ -16,18 +33,20 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    CoreModule
   ],
+  exports: [AngularMaterialModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Examen/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ Examen/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,73 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+  public persons: Person[];
+  public inspectionPlans: InspectionPlan[];
+  public equipments: Equipment[];
+  public calibrations: Calibration[];
+
+  public name: string = "test";
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    this.loadPersons();
+    this.loadInspectionPlans();
+    this.loadEquipments();
+    this.loadCalibrations();
+  }
+
+  loadPersons() {
+    this.http.get<Person[]>(this.baseUrl + 'api/People').subscribe(result => {
+      this.persons = result;
+      console.log(this.persons);
     }, error => console.error(error));
   }
+
+  loadInspectionPlans() {
+    this.http.get<InspectionPlan[]>(this.baseUrl + 'api/InspectionPlans').subscribe(result => {
+      this.inspectionPlans = result;
+      console.log(this.inspectionPlans);
+    }, error => console.error(error));
+  };
+
+  loadEquipments() {
+    this.http.get<Equipment[]>(this.baseUrl + 'api/Equipments').subscribe(result => {
+      this.equipments = result;
+      console.log(this.equipments);
+    }, error => console.error(error));
+  }
+  loadCalibrations() {
+    this.http.get<Calibration[]>(this.baseUrl + 'api/Calibrations').subscribe(result => {
+      this.calibrations = result;
+      console.log(this.calibrations);
+    }, error => console.error(error));
+  }
+
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Person {
+  name: string;
+  surname: string;
+  phoneNumber: string;
+  email: string;
+}
+
+interface InspectionPlan {
+  description: string;
+  temperature: number;
+  humidity: number;
+  pressure: number;
+  personId: number;
+}
+interface Equipment {
+  description: string;
+  equipementType: string;
+  model: string;
+  serialNumber: string;
+  calibrationDate: Date;
+}
+
+interface Calibration {
+  equipmentId: number;
+  personId: number;
 }
